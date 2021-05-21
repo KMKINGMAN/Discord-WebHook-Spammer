be86097459de9ef5d75fc0fe957c585761bfbed2
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-05-22 00:05:13 +0300

    ✨Uploaded Files

---
+++ README.md
@@ -0,0 +1,10 @@
+## 😎 Discord WebHook Spammer
+It is a dedicated discord tool that continuously sends information to the channel discord<br>
+<strong>This Simple Tools Maked By KINGMAN</strong><a href="">MeCodes Server</a>
+<h3>How To Use?</h3>
+<ui>
+    <li>Run The Project Useing This Commands `python3 main.py`</li>
+    <li>insert The Webhook Url</li>
+    <li>insert The Spam Message</li>
+</ui>
+<strong>The tool will simply start working</strong>
---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+import requests
+import pyfiglet
+banner = pyfiglet.figlet_format("KINGMAN")
+print(banner)
+msg = input("Please Insert WebHook Spam Message: ")
+webhook = input("Please Insert WebHook URL: ")
+def spam(msg, webhook):
+    while True:
+        try:
+            data = requests.post(webhook, json={'content': msg})
+            if data.status_code == 204:
+                print(f"Sent MSG {msg}")
+        except:
+            print("Bad Webhook :" + webhook)
+            time.sleep(5)
+            exit()
+kingman_top = 1
+while kingman_top == 1:
+    spam(msg, webhook)
